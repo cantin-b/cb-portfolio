4ddad0d59ae9d18386328b7b2d22ec4aeb31035d
Cantin Bartel <cantinbartel.pro@gmail.com>
2025-03-25 20:25:39 +0100

    disable ESLint and strict TS checks for production build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
 }
+
+module.exports = nextConfig
